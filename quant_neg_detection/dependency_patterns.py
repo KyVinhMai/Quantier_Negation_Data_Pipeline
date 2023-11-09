768848cf515dae52334af1ec120dd302ab805837
Jordan Jin <jinjt@uci.edu>
2023-11-08 23:53:11 -0800

    Adjustments to match all-negations

--- quant_neg_detection/dependency_patterns.py
+++ quant_neg_detection/dependency_patterns.py
@@ -187,3 +187,240 @@ neg_pattern = [
             "RIGHT_ATTRS": {"POS": "PART", "DEP": "neg"}
         },
     ]
+
+def qn_patterns(quantifier: str) -> list[list[dict]]:
+    assert quantifier.lower() in ("all", "every", "each")
+    if quantifier.lower() == "all":
+        quantifier_forms = ["all"]
+    elif quantifier.lower() == "every":
+        quantifier_forms = ["every", "everyone", "everyday", "everyway", "everymen", "everyman", "everybody",
+                            "everydays", "everywoman", "everywomen", "everything", "everywhere", "everyplace"]
+    elif quantifier.lower() == "each":
+        quantifier_forms = ["each"]
+
+    pattern1 = [
+        {
+            "RIGHT_ID": "anchor_aux",
+            "RIGHT_ATTRS": {"POS": {"IN": ["AUX", "VERB"]}}
+        },
+        {
+            "LEFT_ID": "anchor_aux",
+            "REL_OP": ">",
+            "RIGHT_ID": "noun_subject",
+            "RIGHT_ATTRS": {"DEP": {"IN": ["nsubj", "nsubjpass"]}, "LOWER": {"IN": quantifier_forms}},
+        },
+        # {
+        #     "LEFT_ID": "noun_subject",
+        #     "REL_OP": ";",
+        #     "RIGHT_ID": "forbidden",
+        #     "RIGHT_ATTRS": {"LOWER": {"IN": ["almost", "most", "nearly", "y'", "y '"]}, "OP": "!"},
+        # },
+        {
+            "LEFT_ID": "anchor_aux",
+            "REL_OP": ">",
+            "RIGHT_ID": "negation_particle",
+            "RIGHT_ATTRS": {"DEP": "neg"},
+        },
+    ]
+
+    pattern2 = [
+        {
+            "RIGHT_ID": "anchor_aux",
+            "RIGHT_ATTRS": {"POS": {"IN": ["AUX", "VERB"]}}
+        },
+        {
+            "LEFT_ID": "anchor_aux",
+            "REL_OP": ">",
+            "RIGHT_ID": "noun_subject",
+            "RIGHT_ATTRS": {"DEP": {"IN": ["nsubj", "nsubjpass"]}},
+        },
+        {
+            "LEFT_ID": "noun_subject",
+            "REL_OP": ">",
+            "RIGHT_ID": "predet",
+            "RIGHT_ATTRS": {"DEP": {"IN": ["predet", "det"]}, "LOWER": {"IN": quantifier_forms}},
+        },
+        {
+            "LEFT_ID": "anchor_aux",
+            "REL_OP": ">",
+            "RIGHT_ID": "negation_particle",
+            "RIGHT_ATTRS": {"DEP": "neg"},
+        },
+    ]
+
+    # All of our facilities are not sufficiently hardened.
+    pattern3 = [
+        {
+            "RIGHT_ID": "anchor_aux",
+            "RIGHT_ATTRS": {"POS": {"IN": ["AUX", "VERB"]}}
+        },
+        {
+            "LEFT_ID": "anchor_aux",
+            "REL_OP": ">",
+            "RIGHT_ID": "noun_subject",
+            "RIGHT_ATTRS": {"DEP": {"IN": ["nsubj", "nsubjpass"]}, "LOWER": {"IN": quantifier_forms}},
+        },
+        {
+            "LEFT_ID": "anchor_aux",
+            "REL_OP": ">",
+            "RIGHT_ID": "auxpass",
+            "RIGHT_ATTRS": {"DEP": "auxpass"},
+        },
+        {
+            "LEFT_ID": "auxpass",
+            "REL_OP": ">",
+            "RIGHT_ID": "negation_particle",
+            "RIGHT_ATTRS": {"DEP": "neg"},
+        },
+    ]
+
+    # All are not sufficiently hardened.
+    pattern4 = [
+        {
+            "RIGHT_ID": "anchor_aux",
+            "RIGHT_ATTRS": {"POS": {"IN": ["AUX", "VERB"]}}
+        },
+        {
+            "LEFT_ID": "anchor_aux",
+            "REL_OP": ">",
+            "RIGHT_ID": "noun_subject",
+            "RIGHT_ATTRS": {"DEP": {"IN": ["nsubj", "nsubjpass"]}, "LOWER": {"IN": quantifier_forms}},
+        },
+        {
+            "LEFT_ID": "anchor_aux",
+            "REL_OP": ">",
+            "RIGHT_ID": "auxpass",
+            # Including aux and xcomp for DEP decreased false negative rate by 0.010 and increased false positive by 0.025 in all-neg fitting set
+            # Decreased false negative rate by 0.000 and increased false positive by 0.021 in all-neg test set
+            "RIGHT_ATTRS": {"DEP": "auxpass"},
+        },
+        {
+            "LEFT_ID": "auxpass",
+            "REL_OP": ">",
+            "RIGHT_ID": "negation_particle",
+            "RIGHT_ATTRS": {"DEP": "neg"},
+        },
+    ]
+
+    # # The bill and all others did not go forward.
+    # pattern5 = [
+    #     {
+    #         "RIGHT_ID": "anchor_aux",
+    #         "RIGHT_ATTRS": {"POS": {"IN": ["AUX", "VERB"]}}
+    #     },
+    #     {
+    #         "LEFT_ID": "anchor_aux",
+    #         "REL_OP": ">",
+    #         "RIGHT_ID": "noun_subject",
+    #         "RIGHT_ATTRS": {"DEP": {"IN": ["nsubj", "nsubjpass"]}},
+    #     },
+    #     {
+    #         "LEFT_ID": "noun_subject",
+    #         "REL_OP": ">",
+    #         "RIGHT_ID": "nsubj_child",
+    #         "RIGHT_ATTRS": {"DEP": {"IN": ["conj", "appos"]}},
+    #     },
+    #     {
+    #         "LEFT_ID": "nsubj_child",
+    #         "REL_OP": ">",
+    #         "RIGHT_ID": "predet",
+    #         "RIGHT_ATTRS": {"DEP": {"IN": ["predet", "det"]}, "LOWER": {"IN": quantifier_forms}},
+    #     },
+    #     {
+    #         "LEFT_ID": "anchor_aux",
+    #         "REL_OP": ">",
+    #         "RIGHT_ID": "negation_particle",
+    #         "RIGHT_ATTRS": {"DEP": "neg"},
+    #     },
+    # ]
+
+    # All the king's men couldn't put these two back together.
+    pattern6 = [
+        {
+            "RIGHT_ID": "anchor_aux",
+            "RIGHT_ATTRS": {"POS": {"IN": ["AUX", "VERB"]}}
+        },
+        {
+            "LEFT_ID": "anchor_aux",
+            "REL_OP": ">",
+            "RIGHT_ID": "noun_subject",
+            "RIGHT_ATTRS": {"DEP": {"IN": ["nsubj", "nsubjpass"]}},
+        },
+        {
+            "LEFT_ID": "noun_subject",
+            "REL_OP": ">",
+            "RIGHT_ID": "nsubj_child",
+            "RIGHT_ATTRS": {"DEP": {"IN": ["conj", "appos"]}},
+        },
+        {
+            "LEFT_ID": "nsubj_child",
+            "REL_OP": ">",
+            "RIGHT_ID": "poss",
+            "RIGHT_ATTRS": {"DEP": "poss"},
+        },
+        {
+            "LEFT_ID": "poss",
+            "REL_OP": ">",
+            "RIGHT_ID": "predet",
+            "RIGHT_ATTRS": {"DEP": {"IN": ["predet", "det"]}, "LOWER": {"IN": quantifier_forms}},
+        },
+        {
+            "LEFT_ID": "anchor_aux",
+            "REL_OP": ">",
+            "RIGHT_ID": "negation_particle",
+            "RIGHT_ATTRS": {"DEP": "neg"},
+        },
+    ]
+
+    # All of that wasn't well received at all.
+    pattern7 = [
+        {
+            "RIGHT_ID": "anchor_verb",
+            "RIGHT_ATTRS": {"POS": "VERB"}
+        },
+        {
+            "LEFT_ID": "anchor_verb",
+            "REL_OP": ">",
+            "RIGHT_ID": "noun_subject",
+            "RIGHT_ATTRS": {"DEP": {"IN": ["nsubj", "nsubjpass"]}, "LOWER": {"IN": quantifier_forms}},
+        },
+        {
+            "LEFT_ID": "anchor_verb",
+            "REL_OP": ">",
+            "RIGHT_ID": "child_aux",
+            "RIGHT_ATTRS": {"DEP": "aux"},
+        },
+        {
+            "LEFT_ID": "child_aux",
+            "REL_OP": ">",
+            "RIGHT_ID": "negation_particle",
+            "RIGHT_ATTRS": {"DEP": "neg"},
+        },
+    ]
+
+    pattern8 = [
+        {
+            "RIGHT_ID": "anchor_pronoun",
+            "RIGHT_ATTRS": {"POS": "PRON"}
+        },
+        {
+            "LEFT_ID": "anchor_pronoun",
+            "REL_OP": ">",
+            "RIGHT_ID": "noun_subject",
+            "RIGHT_ATTRS": {"DEP": {"IN": ["nsubj", "nsubjpass"]}},
+        },
+        {
+            "LEFT_ID": "noun_subject",
+            "REL_OP": ">",
+            "RIGHT_ID": "nsubj_child",
+            "RIGHT_ATTRS": {"DEP": {"IN": ["predet", "det"]}, "LOWER": {"IN": quantifier_forms}},
+        },
+        {
+            "LEFT_ID": "anchor_pronoun",
+            "REL_OP": ">",
+            "RIGHT_ID": "negation_particle",
+            "RIGHT_ATTRS": {"DEP": "neg"},
+        },
+    ]
+
+    return [pattern1, pattern2, pattern3, pattern4, pattern6, pattern7, pattern8]
