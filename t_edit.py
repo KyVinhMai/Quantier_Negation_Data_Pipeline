0753c98480cc2dcf2e9241757ad3798ee1227631
kyvinh <kyvinhmai@gmail.com>
2024-01-02 10:00:38 -0800

    I'm being silly. Should have input the index information into the sql database

--- t_edit.py
+++ t_edit.py
@@ -1,4 +1,4 @@
-from fastDamerauLevenshtein import damerauLevenshtein
+from fastDamerauLevenshtein import damerauLevenshtein as edit_dist
 from operator import itemgetter
 
 context = "Michael Jackson is who you're talking about. Thank you, yeah - because he's shy. And everybody didn't want to approach him anyway because he's so shy. But he and I would sit and have lunch together."
@@ -7,23 +7,26 @@ match = "everybody didn't want to approach him anyway"
 with open("life_happens.txt") as f:
     transcript = f.read().split(".")
 
-scores = []
-# Search for the maximum score
-for index, sentence in enumerate(transcript):
-    score = damerauLevenshtein(match, sentence, similarity=True)
-    print(score, sentence) # expected result: 2.0
-    scores.append([match, sentence, index, score])
+def edit_distance(match, context, transcript_list):
+    scores = []
+    rankings = ()
+    context_ranks = []
 
-rankings = sorted(scores, key=itemgetter(3), reverse=True)
-highest = rankings[0]
+    for index, sentence in enumerate(transcript):
+        score = edit_dist(match, sentence, similarity=True)
+        print(score, sentence)  # expected result: 2.0
+        scores.append([match, sentence, index, score])
 
-print(highest)
+    rankings = sorted(scores, key=itemgetter(3), reverse=True)[:3] # Top 3
 
-#Checking for validity and tiebreaking
-match_i = highest[2]
-context_start = match_i - 4 if match_i != 0 else 0
-context_end = match_i + 1 if match_i != len(transcript) else len(transcript)
+    for candidate in rankings:
+        match_i = candidate[2]
+        context_start = match_i - 3 if match_i != 0 else 0
+        context_end = match_i + 1 if match_i != len(transcript) else len(transcript)
+        result = edit_dist(context, "".join(transcript[context_start:context_end + 1]), similarity=True)
+        context_ranks.append((context_start, context_end, result))
 
-print(transcript[context_start:context_end + 1])
+    print(context_ranks)
 
-print(damerauLevenshtein(context, transcript[context_start:context_end + 1], similarity=True))
+
+edit_distance(match, context, transcript)
